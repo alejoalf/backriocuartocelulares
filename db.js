75449b12295a90e2a92d83ec3254171672616d26
alejoalf <alejoalfonsoaa@gmail.com>
2025-07-18 12:28:37 -0300

    port agregado 1

--- db.js
+++ db.js
@@ -6,9 +6,9 @@ const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
-  process.env.DB_PORT,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     logging: true,
   }
