dda508811fbc5d8f8e94643853dea736af4290db
alejoalf <alejoalfonsoaa@gmail.com>
2025-07-18 16:05:29 -0300

    cambios db

--- db.js
+++ db.js
@@ -8,9 +8,18 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres', // <-- ¡ESTO ES CLAVE!
-    logging: false,      // o una función, o false para evitar el warning
+    port: process.env.DB_PORT, // ¡Excelente que agregaste DB_PORT!
+    dialect: 'postgres',
+    logging: false, // o una función, o false para evitar el warning
+
+    // **** ¡AGREGA ESTO! ****
+    dialectOptions: {
+      ssl: {
+        require: true,           // Forzar el uso de SSL
+        rejectUnauthorized: false // Ignorar la validación del certificado (necesario en Render)
+      }
+    }
+    // **********************
   }
 );
 
